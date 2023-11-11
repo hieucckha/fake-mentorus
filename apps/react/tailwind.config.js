fa45b9851b16ee58f5a76b18b28eb152d0ea0e63
hieucckha <hieucckha@gmail.com>
2023-11-11 23:47:01 +0700

    Reconfigure tailwindcss

--- apps/react/tailwind.config.js
+++ apps/react/tailwind.config.js
@@ -1,8 +1,13 @@
+/* eslint-disable @typescript-eslint/no-require-imports */
+const { join } = require('path');
+
+const { createGlobPatternsForDependencies } = require('@nx/react/tailwind');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-    './public/index.html',
+    join(__dirname, '{src,pages,components,app}/**/*!(*.stories|*.spec).{ts,tsx,html}'),
+    ...createGlobPatternsForDependencies(__dirname),
   ],
   theme: {
     extend: {},
