2aea9a10e67249384628bd5f842d17b8f98fb477
nguyenhau2506 <nvhau20@clc.fitus.edu.vn>
2023-11-12 07:53:51 +0700

    Login/signup page view

--- apps/react/src/screens/loginPage.tsx
+++ apps/react/src/screens/loginPage.tsx
@@ -1,8 +1,14 @@
+import { useState } from 'react';
+
 /**
  *
  * @returns LoginPage.
  */
 export default function loginPage() {
+  const [action, setAction] = useState('login');
+  const handleChangeAction = () => {
+    setAction(action === 'login' ? 'register' : 'login');
+  };
   return (
     <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-sm">
@@ -12,7 +18,7 @@ export default function loginPage() {
           alt="Your Company"
         />
         <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-          Sign in to your account
+          {action === 'login' ? 'Sign in to your account' : 'Create a new account'}
         </h2>
       </div>
       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
@@ -39,11 +45,7 @@ export default function loginPage() {
               <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
                 Password
               </label>
-              <div className="text-sm">
-                <a href="#" className="font-semibold text-indigo-600 hover:text-indigo-500">
-                  Forgot password?
-                </a>
-              </div>
+
             </div>
             <div className="mt-2">
               <input
@@ -58,6 +60,29 @@ export default function loginPage() {
               />
             </div>
           </div>
+          {action === 'register' ? (
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">
+                Confirm password
+                </label>
+
+              </div>
+              <div className="mt-2">
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="block w-full rounded-md border-0 py-1.5 text-gray-900
+                shadow-sm ring-1 ring-inset ring-gray-300
+                 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+          ) :
+            null}
           <div>
             <button
               type="submit"
@@ -69,10 +94,10 @@ export default function loginPage() {
             </button>
           </div>
         </form>
-        <p className="mt-10 text-center text-sm text-gray-500">
-          Not a member?
-          <a href="#" className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
-            Start a 14 day free trial
+        <p className="mt-10 text-end text-sm text-gray-500">
+          {action === 'login' ? 'Don`t have an account' : 'Already have an account?'}
+          <a onClick={handleChangeAction} className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500">
+            {action === 'login' ? 'Sign up' : 'Sign in'}
           </a>
         </p>
       </div>
