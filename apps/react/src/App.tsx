40c00e771fe952c0cb96281e645161a55c93be15
Caby <vothanhsuong123@gmail.com>
2023-11-13 15:59:04 +0700

    Fix warning css and add AOS

--- apps/react/src/App.tsx
+++ apps/react/src/App.tsx
@@ -1,17 +1,49 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes, useLocation } from 'react-router-dom';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+import { focusHandling } from 'cruip-js-toolkit';
 
 import Login from './pages/SignIn';
-import Home from './pages/Home';
+import Landing from './pages/Landing';
 
 /**
  * App.
  * @returns App.
  */
-export const App: FC = () => (
-  <Routes>
-    <Route path='/' element={<Home />} />
-    <Route path="/sign-in" element={<Login />} />
-  </Routes>
-);
+export const App: FC = () => {
+
+  const location = useLocation();
+
+  useEffect(() => {
+    AOS.init({
+      once: true,
+      disable: 'phone',
+      duration: 700,
+      easing: 'ease-out-cubic',
+    });
+  });
+
+  // triggered on route change
+  useEffect(() => {
+    const root = document.querySelector('html');
+
+    if (root) {
+      root.style.scrollBehavior = 'auto';
+      window.scroll({ top: 0 });
+      root.style.scrollBehavior = '';
+      focusHandling('outline');
+    }
+
+  }, [location.pathname]);
+
+  return (
+    <Routes>
+      <Route path='/' element={<Landing />} />
+      <Route path="/sign-in" element={<Login />} />
+    </Routes>
+  );
+};
