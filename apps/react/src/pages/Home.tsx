0222f1f72d99768fd4b482dbdec382852bcf6db0
Capy <vothanhsuong123@gmail.com>
2023-11-13 14:22:39 +0700

    first push

--- apps/react/src/pages/Home.tsx
+++ apps/react/src/pages/Home.tsx
@@ -1,14 +1,32 @@
 import { FC } from 'react';
-import { Link } from 'react-router-dom';
+
+// import { Link } from 'react-router-dom';
+
+import Header from '../partials/Header';
+import Footer from '../partials/Footer';
 
 /**
  * Home page.
  */
 const Home: FC = () => (
-  <>
-    <h1>Hello World</h1>
-    <Link to="/sign-in">Sign in</Link>
-  </>
+  <div className="flex flex-col min-h-screen overflow-hidden">
+    <Header />
+
+    {/*  Page content */}
+    <main className="flex-grow">
+
+      {/*  Page sections */}
+      {/* <HeroHome />
+      <FeaturesHome />
+      <FeaturesBlocks />
+      <Testimonials />
+      <Newsletter /> */}
+
+    </main>
+
+    {/*  Site footer */}
+    <Footer />
+  </div>
 );
 
 export default Home;
