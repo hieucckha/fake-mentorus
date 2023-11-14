d519ae7030baef8bc004ac2ea93b19addec2f321
nguyenhau2506 <75925835+nguyenhau2506@users.noreply.github.com>
2023-11-14 13:35:17 +0700

    Update App.tsx
    add public and private route

--- apps/react/src/App.tsx
+++ apps/react/src/App.tsx
@@ -1,5 +1,5 @@
 import { FC, useEffect } from 'react';
-import { Route, Routes, useLocation } from 'react-router-dom';
+import { Navigate, Route, Routes, useLocation } from 'react-router-dom';
 import AOS from 'aos';
 import 'aos/dist/aos.css';
 import { focusHandling } from 'cruip-js-toolkit';
@@ -8,6 +8,8 @@ import Login from './pages/SignIn';
 import NotFound from './pages/NotFound';
 import Register from './pages/SignUp';
 import Landing from './pages/Landing';
+
+import localStorageService from './services/localStorage.service';
 
 /**
  * App.
@@ -39,12 +41,24 @@ export const App: FC = () => {
 
   }, [location.pathname]);
 
+  const privateRoute = (element: JSX.Element): JSX.Element => {
+    const token = localStorageService.getItem('auth');
+    if (token) {
+      return element;
+    }
+    return <Navigate to="/sign-in" />;
+  };
+
+  const publicRoute = (element: JSX.Element): JSX.Element => element;
+
   return (
-    <Routes>
-      <Route path='/' element={<Landing />} />
-      <Route path="/sign-in" element={<Login />} />
-      <Route path="/sign-up" element={<Register />} />
-      <Route path='*' element={<NotFound />} />
+    <Routes >
+      <Route path="/" element={privateRoute(<Landing />)} />
+      <Route path="/sign-in" element={publicRoute(<Login />)} />
+      <Route path="/sign-up" element={publicRoute(<Register />)} />
+
+      <Route path="*" element={<NotFound />} />
     </Routes>
   );
+
 };
