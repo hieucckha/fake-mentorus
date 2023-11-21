d18eb953e967cb68a765522d580ccefb90b41d24
hieucckha <hieucckha@gmail.com>
2023-11-21 09:13:29 +0700

    Add backend

---
+++ src/backend/SomeSandwich.FakeMentorus.Infrastructure.Abstractions/Interfaces/IAppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SomeSandwich.FakeMentorus.Domain.Users;
+
+namespace SomeSandwich.FakeMentorus.Infrastructure.Abstractions.Interfaces;
+
+/// <summary>
+/// Application abstraction for unit of work.
+/// </summary>
+public interface IAppDbContext : IDbContextWithSets, IDisposable
+{
+    /// <summary>
+    /// Users.
+    /// </summary>
+    DbSet<User> Users { get; }
+}
