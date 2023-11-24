7d2dfe995a7e014aef1151c6d246952bc0315b98
hieucckha <hieucckha@gmail.com>
2023-11-24 20:32:18 +0700

    Remove nx of react project

---
+++ src/frontend/src/main.tsx
@@ -0,0 +1,18 @@
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router-dom";
+
+import "flowbite";
+import "./index.scss";
+import router from "./router";
+
+const rootElement: HTMLElement | null = document.querySelector("#root");
+if (rootElement == null) {
+	throw new Error("Failed to find root element");
+}
+
+ReactDOM.createRoot(rootElement).render(
+	// <StrictMode>
+	<RouterProvider router={router} />
+
+	// </StrictMode>,
+);
