72909ed6a2bc9f87cee86ba6283cc096eb9226d4
nguyenhau2506 <nvhau20@clc.fitus.edu.vn>
2023-11-26 00:45:17 +0700

    doing create class view

--- src/frontend/src/hooks/auth.tsx
+++ src/frontend/src/hooks/auth.tsx
@@ -1,10 +1,10 @@
 import { useNavigate } from 'react-router-dom';
 
-import { UseQueryResult } from '@tanstack/react-query';
+import type { UseQueryResult } from '@tanstack/react-query';
 
 import useAuthQuery from '../api/store/auth/queries';
 import localStorageService from '../services/localStorage.service';
-import { UserProfileDto } from '../api/store/auth/interface';
+import type { UserProfileDto } from '../api/store/auth/interface';
 
 /**
  * Auth hook.
@@ -13,6 +13,7 @@ import { UserProfileDto } from '../api/store/auth/interface';
 const useAuth = (): Omit<UseQueryResult<UserProfileDto>, 'error' | 'isError'> => {
    const queryData = useAuthQuery();
    if (queryData.isError && queryData.error) {
+    // eslint-disable-next-line react-hooks/rules-of-hooks
     const navigate = useNavigate();
       localStorageService.removeItem('auth');
      navigate('/sign-in');
