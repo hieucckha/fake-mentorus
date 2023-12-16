98bbdf75b85a1dd33cff10b74fa22eb9f1e70166
hieucckha <hieucckha@gmail.com>
2023-12-16 21:14:05 +0700

    Add skeleton of import student list template feature

---
+++ src/backend/SomeSandwich.FakeMentorus.UseCases/Grade/ImportStudentList/ImportStudentListCommandHandler.cs
@@ -0,0 +1,31 @@
+﻿using MediatR;
+using Microsoft.Extensions.Logging;
+using SomeSandwich.FakeMentorus.Infrastructure.Abstractions.Interfaces;
+
+namespace SomeSandwich.FakeMentorus.UseCases.Grade.ImportStudentList;
+
+/// <summary>
+/// Handler for <see cref="ImportStudentListCommand"/>.
+/// </summary>
+internal class ImportStudentListCommandHandler : IRequestHandler<ImportStudentListCommand>
+{
+    private readonly ILogger<ImportStudentListCommandHandler> logger;
+    private readonly IAppDbContext appDbContext;
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    /// <param name="logger"></param>
+    /// <param name="appDbContext"></param>
+    public ImportStudentListCommandHandler(ILogger<ImportStudentListCommandHandler> logger, IAppDbContext appDbContext)
+    {
+        this.logger = logger;
+        this.appDbContext = appDbContext;
+    }
+
+    /// <inheritdoc />
+    public async Task Handle(ImportStudentListCommand request, CancellationToken cancellationToken)
+    {
+
+    }
+}
