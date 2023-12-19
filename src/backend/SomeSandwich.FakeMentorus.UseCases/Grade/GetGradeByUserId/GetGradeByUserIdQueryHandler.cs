c540d9c090f9bebf3c7ab837fd0f2ef9f0a3b0c0
capybbv <vothanhsuong123@gmail.com>
2023-12-20 00:14:22 +0700

    fix get grade type is deleted, get grade by student id

---
+++ src/backend/SomeSandwich.FakeMentorus.UseCases/Grade/GetGradeByUserId/GetGradeByUserIdQueryHandler.cs
@@ -0,0 +1,69 @@
+using AutoMapper;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using SomeSandwich.FakeMentorus.Infrastructure.Abstractions.Interfaces;
+using SomeSandwich.FakeMentorus.UseCases.Grade.Common;
+using SomeSandwich.FakeMentorus.UseCases.GradeComposition.Common;
+
+namespace SomeSandwich.FakeMentorus.UseCases.Grade.GetGradeByUserId;
+
+public class GetGradeByUserIdQueryHandler : IRequestHandler<GetGradeByUserIdQuery, GetGradeByUserIdResult>
+{
+    private readonly IAppDbContext appDbContext;
+    private readonly IMapper mapper;
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    /// <param name="appDbContext"></param>
+    /// <param name="mapper"></param>
+    public GetGradeByUserIdQueryHandler(IAppDbContext appDbContext, IMapper mapper)
+    {
+        this.appDbContext = appDbContext;
+        this.mapper = mapper;
+    }
+
+    /// <inheritdoc />
+    public async Task<GetGradeByUserIdResult> Handle(GetGradeByUserIdQuery query, CancellationToken cancellationToken)
+    {
+        var gradeComposites = await appDbContext.GradeCompositions
+            .Include(e => e.Grades)
+            .Where(e => e.CourseId == query.CourseId && e.IsDeleted == false)
+            .OrderBy(e => e.GradeScale)
+            .ThenBy(e => e.Id)
+            .ToListAsync(cancellationToken);
+
+        var gradeCells = gradeComposites.Select(e =>
+        {
+            var grade = e.Grades.FirstOrDefault(g => g.GradeCompositionId == e.Id && g.StudentId == query.StudentId);
+            return new GradeCellDto()
+            {
+                Id = grade?.Id,
+                GradeCompositionId = e.Id,
+                GradeValue = grade?.GradeValue,
+                IsRequested = grade?.IsRequested ?? false,
+            };
+        }).ToList();
+
+        var student = await appDbContext.StudentInfos
+            .Include(e => e.Student).ThenInclude(f => f.User)
+            .Where(e => e.StudentId == query.StudentId && e.CourseId == query.CourseId)
+            .FirstOrDefaultAsync(cancellationToken);
+        var studentName = student?.Name ?? "";
+        var userId = student?.Student?.User?.Id;
+
+        var gradeCell = new GradeCell()
+        {
+            StudentId = query.StudentId, StudentName = studentName, UserId = userId, GradeDto = gradeCells,
+        };
+
+        var result = new GetGradeByUserIdResult()
+        {
+            CourseId = query.CourseId,
+            GradeCompositionDtos = gradeComposites.Select(e => mapper.Map<GradeCompositionDto>(e)).ToList(),
+            Student = gradeCell,
+        };
+
+        return result;
+    }
+}
