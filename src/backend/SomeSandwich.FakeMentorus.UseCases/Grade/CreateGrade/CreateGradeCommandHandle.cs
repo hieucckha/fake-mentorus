f264a8e8f9bd8520eb271ab73ff9ffda0c4746e8
capybbv <vothanhsuong123@gmail.com>
2023-12-21 13:14:19 +0700

    edit grade

--- src/backend/SomeSandwich.FakeMentorus.UseCases/Grade/CreateGrade/CreateGradeCommandHandle.cs
+++ src/backend/SomeSandwich.FakeMentorus.UseCases/Grade/CreateGrade/CreateGradeCommandHandle.cs
@@ -29,12 +29,26 @@ internal class CreateGradeCommandHandle : IRequestHandler<CreateGradeCommand, Gr
     /// <inheritdoc />
     public async Task<GradeDto> Handle(CreateGradeCommand request, CancellationToken cancellationToken)
     {
-        var check = await dbContext.Grades.AnyAsync(
+        var student = await dbContext.StudentInfos
+            .Include(si => si.Course)
+            .ThenInclude(c => c.GradeCompositions)
+            .Where(si => si.StudentId == request.StudentId)
+            .Where(si => si.Course.GradeCompositions.Any(gc => gc.Id == request.GradeCompositionId))
+            .FirstOrDefaultAsync(cancellationToken);
+
+        var gradeOld = await dbContext.Grades.FirstOrDefaultAsync(
             e => e.GradeCompositionId == request.GradeCompositionId && e.StudentId == request.StudentId,
             cancellationToken);
-        if (check)
+        if (gradeOld != null)
         {
-            throw new DomainException("Grade already exists");
+            // throw new DomainException("Grade already exists");
+            gradeOld.GradeValue = request.GradeValue;
+            gradeOld.UpdatedAt = DateTime.UtcNow;
+            await dbContext.SaveChangesAsync(cancellationToken);
+            var result1 = mapper.Map<GradeDto>(gradeOld);
+            result1.StudentName = student?.Name ?? "";
+
+            return result1;
         }
 
         var grade = new Domain.Grade.Grade
@@ -48,12 +62,7 @@ internal class CreateGradeCommandHandle : IRequestHandler<CreateGradeCommand, Gr
         await dbContext.SaveChangesAsync(cancellationToken);
 
         var result = mapper.Map<GradeDto>(grade);
-        var student = await dbContext.StudentInfos
-            .Include(si => si.Course)
-            .ThenInclude(c => c.GradeCompositions)
-            .Where(si => si.StudentId == request.StudentId)
-            .Where(si => si.Course.GradeCompositions.Any(gc => gc.Id == request.GradeCompositionId))
-            .FirstOrDefaultAsync(cancellationToken);
+
         result.StudentName = student?.Name ?? "";
 
 
