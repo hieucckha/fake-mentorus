aabde5bbfc7f895f13c2e339e12e0b70aa1662f0
capybbv <vothanhsuong123@gmail.com>
2023-12-25 19:00:02 +0700

    Fix sidebar active menu

--- src/frontend/src/partials/Sidebar.tsx
+++ src/frontend/src/partials/Sidebar.tsx
@@ -6,7 +6,7 @@ import {
 	UsergroupAddOutlined,
 } from "@ant-design/icons";
 import type { MenuProps } from "antd";
-import {  Menu, Skeleton } from "antd";
+import { Menu, Skeleton } from "antd";
 import { NavLink, useLocation } from "react-router-dom";
 import classQuery from "../api/store/class/queries";
 import useAuth from "../hooks/auth";
@@ -34,53 +34,94 @@ const fontSizeIcon = "20px";
 const fontSizeMenu = "16px";
 
 const Sidebar: React.FC = () => {
-	// const [collapsed, setCollapsed] = useState(false);
-	const { data: user } = useAuth();
-
+	const { data: user, isLoading: isProfileLoading } = useAuth();
 	const { data, isLoading } = classQuery(user?.id ?? -1);
 	const { pathname } = useLocation();
-	const getSelectedKey = (pathname: string) : string[]=>{
-		var splitPath = pathname.split('/');
-		if(splitPath.length>1){
-			return [[splitPath[1],splitPath[2]].join('/')]
+
+	const getSelectedKey = (pathname: string): string[] => {
+		const splitPath = pathname.split("/").filter((item) => item !== "");
+		if (splitPath.length === 1) {
+			return splitPath;
 		}
-		return ['']
-	}
-	const getDefaultOpenKey = (pathname: string) : string[]=>{
-		var splitPath = pathname.split('/');
-		return splitPath.includes('class') ? ['classes'] : [];
-	}
-	var itemsMenu : MenuProps['items'] = []
-	if(isLoading){
-		itemsMenu = [
-			getItem(<NavLink style={{ fontSize: fontSizeMenu}} to={"home"}>Home</NavLink>, "home", <HomeOutlined style={{ fontSize: fontSizeIcon}}/>),
-		
+
+		return [splitPath.slice(0, 2).join("/")];
+	};
+
+	const getDefaultOpenClass = (pathname: string): string[] => {
+		const splitPath = pathname.split("/").filter((item) => item !== "");
+		if (splitPath.includes("admin")) {
+			return [];
+		}
+
+		if (splitPath.includes("class")) {
+			return ["classes"];
+		}
+	};
+
+	const menuBuilder = (): MenuProps["items"] => {
+		let userMenu = [
 			getItem(
-				<div className="flex flex-row items-center gap-x-2">
-					<Skeleton.Input className="w-14 h-6" active={true} size={"small"} />
-				</div>,'skeleton',<Skeleton.Avatar className="w-14 h-8" active={true} size={"default"} shape={"circle"} />),
-		]
-	}else if(data){
-		itemsMenu = [
-			getItem(<NavLink style={{ fontSize: fontSizeMenu}} to={"home"}>Home</NavLink>, "home", <HomeOutlined style={{ fontSize: fontSizeIcon}}/>),
-		
-			getItem('Class', "classes", <BookOutlined style={{ fontSize: fontSizeIcon}} />, 
-				data?.map((item)=>
+				<NavLink style={{ fontSize: fontSizeMenu }} to={"home"}>
+					Home
+				</NavLink>,
+				"home",
+				<HomeOutlined style={{ fontSize: fontSizeIcon }} />
+			),
+		];
+
+		if (isLoading || isProfileLoading) {
+			userMenu.push(
+				getItem(
+					<div className="flex flex-row items-center gap-x-2">
+						<Skeleton.Input className="w-14 h-6" active={true} size={"small"} />
+					</div>,
+					"skeleton",
+					<Skeleton.Avatar
+						className="w-14 h-8"
+						active={true}
+						size={"default"}
+						shape={"circle"}
+					/>
+				)
+			);
+			return userMenu;
+		}
+
+		if (user?.role === UserRole.Admin) {
+			userMenu = [
+				getItem(
+					<NavLink style={{ fontSize: fontSizeMenu }} to={"/admin/classes"}>
+						Management class
+					</NavLink>,
+					"admin/classes",
+					<ControlOutlined style={{ fontSize: fontSizeIcon }} />
+				),
+				getItem(
+					<NavLink style={{ fontSize: fontSizeMenu }} to={"/admin/users"}>
+						Management user
+					</NavLink>,
+					"admin/users",
+					<UsergroupAddOutlined style={{ fontSize: fontSizeIcon }} />
+				),
+			];
+			return userMenu;
+		}
+
+		userMenu.push(
+			getItem(
+				"Class",
+				"classes",
+				<BookOutlined style={{ fontSize: fontSizeIcon }} />,
+				data?.map((item) =>
 					getItem(
 						<NavLink to={`class/${item.id}/overview`}>{item.name}</NavLink>,
 						`class/${item.id}`
-					  )
+					)
 				)
-			),
-		]
-	}
-	if(user?.role === UserRole.Admin){
-		itemsMenu = [];
-		itemsMenu.push(getItem(<NavLink style={{ fontSize: fontSizeMenu}} to={"/admin/classes"}>Management class</NavLink>, "Management class", <ControlOutlined style={{ fontSize: fontSizeIcon}}/>))
-		itemsMenu.push(getItem(<NavLink style={{ fontSize: fontSizeMenu}} to={"/admin/users"}>Management user</NavLink>, "Management user", <UsergroupAddOutlined style={{ fontSize: fontSizeIcon}}/>))
-
-	}
-	
+			)
+		);
+		return userMenu;
+	};
 
 	return (
 		<div
@@ -91,11 +132,11 @@ const Sidebar: React.FC = () => {
 				className="mt-0"
 				defaultSelectedKeys={getSelectedKey(pathname)}
 				selectedKeys={getSelectedKey(pathname)}
-				defaultOpenKeys={getDefaultOpenKey(pathname)}
+				defaultOpenKeys={getDefaultOpenClass(pathname)}
 				mode="inline"
 				theme="light"
 				inlineCollapsed={false}
-				items={itemsMenu}
+				items={menuBuilder()}
 			/>
 		</div>
 	);
